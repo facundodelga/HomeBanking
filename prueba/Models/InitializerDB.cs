986aea32e358f1aef7ad58f307aac54270e91002
facundodelga <128828081+facundodelga@users.noreply.github.com>
2024-05-30 12:24:13 -0300

    pongo datos de prueba

--- prueba/Models/InitializerDB.cs
+++ prueba/Models/InitializerDB.cs
@@ -2,6 +2,8 @@
 
 namespace prueba.Models {
     public class InitializerDB {
+
+        
         public static void Main(HomeBankingContext context) {
 
 
@@ -21,10 +23,105 @@ namespace prueba.Models {
             };
 
             if (!context.Accounts.Any()) {
-                var cliente = context.Clients.FirstOrDefault(cl => cl.Email == "vcoronado@gmail.com");
+                creoCuentas(context);
+            }
 
-                if (cliente != null) { //si encuentro el cliente
+            if (!context.Transactions.Any()) {
+
+                creoTransacciones(context);
+            }
+
+            if (!context.Loans.Any()) {
+                creoPrestamos(context);
+
+                var client1 = context.Clients.FirstOrDefault(c => c.Email == "vcoronado@gmail.com");
+                if (client1 != null) {
+                    
+                    var loan1 = context.Loans.FirstOrDefault(l => l.Name == "Hipotecario");
+                    if (loan1 != null) {
+                        var clientLoan1 = new ClientLoan {
+                            Amount = 400000,
+                            ClientId = client1.Id,
+                            LoanId = loan1.Id,
+                            Payments = "60"
+                        };
+                        context.ClientLoans.Add(clientLoan1);
+                    }
+
+                    var loan2 = context.Loans.FirstOrDefault(l => l.Name == "Personal");
+                    if (loan2 != null) {
+                        var clientLoan2 = new ClientLoan {
+                            Amount = 50000,
+                            ClientId = client1.Id,
+                            LoanId = loan2.Id,
+                            Payments = "12"
+                        };
+                        context.ClientLoans.Add(clientLoan2);
+                    }
+
+                    var loan3 = context.Loans.FirstOrDefault(l => l.Name == "Automotriz");
+                    if (loan3 != null) {
+                        var clientLoan3 = new ClientLoan {
+                            Amount = 100000,
+                            ClientId = client1.Id,
+                            LoanId = loan3.Id,
+                            Payments = "24"
+                        };
+                        context.ClientLoans.Add(clientLoan3);
+                    }
+                }
+
+                var client2 = context.Clients.FirstOrDefault(c => c.Email == "laureano@mindhub.com");
+                if (client2 != null) {
                     var accounts = new Account[] {
+
+                        new Account{Number = "LAN001",
+                            CreationDate=DateTime.Now,
+                            ClientId = client2.Id,
+                            Balance=300000
+                        },
+
+                        new Account{Number = "LAN002",
+                            CreationDate=DateTime.Now.AddMonths(3),
+                            ClientId = client2.Id,
+                            Balance=1000
+                        },
+
+                        new Account{Number = "LAN003",
+                            CreationDate=DateTime.Now.AddMonths(12),
+                            ClientId = client2.Id,
+                            Balance=50000
+                        }
+
+                    };
+
+                    context.Accounts.AddRange(accounts);
+                    //context.SaveChanges();
+
+                    var loan3 = context.Loans.FirstOrDefault(l => l.Name == "Automotriz");
+                    if (loan3 != null) {
+                        var clientLoan4 = new ClientLoan {
+                            Amount = 1000000,
+                            ClientId = client2.Id,
+                            LoanId = loan3.Id,
+                            Payments = "12"
+                        };
+                        context.ClientLoans.Add(clientLoan4);
+                    }
+
+                    
+                }
+                context.SaveChanges();
+
+            }
+
+        }
+
+        public static void creoCuentas(HomeBankingContext context) {
+            var cliente = context.Clients.FirstOrDefault(cl => cl.Email == "vcoronado@gmail.com");
+
+            if (cliente != null) { //si encuentro el cliente
+                var accounts = new Account[] {
 
                         new Account{Number = "VIN001",
                             CreationDate=DateTime.Now,
@@ -40,17 +137,17 @@ namespace prueba.Models {
 
                     };
 
-                    context.Accounts.AddRange(accounts);
-                    context.SaveChanges();
+                context.Accounts.AddRange(accounts);
+                context.SaveChanges();
 
-                }
             }
+        }
 
-            if (!context.Transactions.Any()) {
-                var cuenta = context.Accounts.FirstOrDefault(ac => ac.Number == "VIN001");
+        public static void creoTransacciones(HomeBankingContext context) {
+            var cuenta = context.Accounts.FirstOrDefault(ac => ac.Number == "VIN001");
 
-                if (cuenta != null) {
-                    var transacciones = new Transaction[] {
+            if (cuenta != null) {
+                var transacciones = new Transaction[] {
                         new Transaction{
                             Amount = 1000,
                             Type = TransactionType.CREDIT,
@@ -84,12 +181,24 @@ namespace prueba.Models {
                         }
                     };
 
-                    context.Transactions.AddRange(transacciones);
-                    context.SaveChanges();
-                }
+                context.Transactions.AddRange(transacciones);
+                context.SaveChanges();
             }
-
         }
+
+        public static void creoPrestamos(HomeBankingContext context) {
+            var loans = new Loan[] {
+                    new Loan { Name = "Hipotecario", MaxAmount = 40000000, Payments = "12,24,36,48,60" },
+                    new Loan { Name = "Personal", MaxAmount = 22000000, Payments = "6,12,24" },
+                    new Loan { Name = "Automotriz", MaxAmount = 3000000, Payments = "6,12,24,36" },
+                    new Loan { Name = "Prendarios", MaxAmount = 4000000, Payments = "6,12,24,36" },
+                    new Loan { Name = "Estudios", MaxAmount = 10000000, Payments = "6,12,24,36,48,60" }
+                };
+            //agrego los tipos de prestamo
+            context.Loans.AddRange(loans);
+            context.SaveChanges();
+        }
+
 
     }
 }
