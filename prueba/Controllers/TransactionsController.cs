2b1eefe47cac0896f53b7b5c040708b2114d03f6
facundodelga <128828081+facundodelga@users.noreply.github.com>
2024-05-30 08:29:22 -0300

    Create TransactionsController.cs

---
+++ prueba/Controllers/TransactionsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBanking.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase {
+    }
+}
