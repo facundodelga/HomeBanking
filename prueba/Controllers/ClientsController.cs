fceac2b75e8dfb2cb1153e1ff1a29d718a86be4b
facundodelga <128828081+facundodelga@users.noreply.github.com>
2024-06-03 11:21:30 -0300

    agrego current en Client y wwwroot

--- prueba/Controllers/ClientsController.cs
+++ prueba/Controllers/ClientsController.cs
@@ -44,7 +44,29 @@ namespace prueba.Controllers {
                 return Ok(clientDTO);
             }
             catch (Exception ex) {
-                
+
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("current")]
+        public IActionResult GetCurrent() {
+            try {
+                string email = User.FindFirst("Client") != null ? User.FindFirst("Client").Value : string.Empty;
+                if (email == string.Empty) {
+                    return Forbid();
+                }
+
+                Client client = _clientRepository.FindByEmail(email);
+
+                if (client == null) {
+                    return Forbid();
+                }
+
+                var clientDTO = new ClientDTO(client);
+                return Ok(clientDTO);
+            }
+            catch (Exception ex) {
                 return StatusCode(500, ex.Message);
             }
         }
