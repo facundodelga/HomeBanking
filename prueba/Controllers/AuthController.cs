bc11947956f733bc3b8833e94262b2761496dc06
facundodelga <128828081+facundodelga@users.noreply.github.com>
2024-06-03 11:16:58 -0300

    actualizo logica AuthController y agrego LoginDTO

--- prueba/Controllers/AuthController.cs
+++ prueba/Controllers/AuthController.cs
@@ -1,9 +1,62 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using prueba.Models;
+using prueba.Repository;
+using System.Security.Claims;
+using HomeBanking.DTOS;
 
 namespace HomeBanking.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase {
+        private IClientRepository _clientRepository;
+
+        public AuthController(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO client) {
+            try {
+                Client user = _clientRepository.FindByEmail(client.Email);
+                if (user == null || !String.Equals(user.Password, client.Password))
+                    return Unauthorized();
+
+                var claims = new List<Claim>
+                {
+                    new Claim("Client", user.Email),
+                };
+
+                var claimsIdentity = new ClaimsIdentity(
+                    claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme
+                    );
+
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity));
+
+                return Ok();
+
+            }
+            catch (Exception ex) {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout() {
+            try {
+                await HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+                return Ok();
+            }
+            catch (Exception ex) {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
